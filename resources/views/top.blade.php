16d1bddfde3c5f8a58183bd5fa27757c7fce731c
unimoa <moa.uxu@gmail.com>
2021-08-16 21:38:49 +0900

    TOPページ、CSS、画像の追加

---
+++ resources/views/top.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>top</title>
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+  <div class="content">
+    <div class="main-visual">
+      <img src="{{ asset('image/mv.gif') }}" alt="">
+    </div>
+    <section>
+      <h1>お知らせ</h1>
+      <ul>
+        <li>コロナ対策について</li>
+        <li>お申し込み再開しました！</li>
+        <li>お申し込みの一時休止</li>
+      </ul>
+    </section>
+    <section>
+      <a href="">
+        <img src="/public/image/topF.png" alt="">
+      </a>
+      <a href="">
+        <img src="/public/image/topO.png" alt="">
+      </a>
+    </section>
+  </div>
+</body>
+</html>
