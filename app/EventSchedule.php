b6ce38acdfc9abfe58800fcfbda6414c0831f485
sakusakuhongoroo <80045643+sakusakuhongoroo@users.noreply.github.com>
2021-08-25 11:05:31 +0800

    メソッド削除　10-18行目

--- app/EventSchedule.php
+++ app/EventSchedule.php
@@ -7,14 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class EventSchedule extends Model
 {
     //
-    function schedule()
-    {
-        return $this->belongsTo('App\Schedule');
-    }
-
-    function event()
-    {
-        return $this->belongsTo('App\Event');
-    }
-
+ 
 }
