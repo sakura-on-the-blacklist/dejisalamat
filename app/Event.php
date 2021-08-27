05e26794ee780f3b113d590c5ebb02077d15150b
sakusakuhongoroo <80045643+sakusakuhongoroo@users.noreply.github.com>
2021-08-27 18:21:21 +0800

    Update Event.php

--- app/Event.php
+++ app/Event.php
@@ -6,14 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    function index()
-    {
-  
-        $event = Event::where('country_id', 1,2) ->get();
-       
-        return view('events.show',['events' => $event]);
-    }
-
+    
     function comment_ranks()
     {
         return $this->hasMany('App\Comment')
